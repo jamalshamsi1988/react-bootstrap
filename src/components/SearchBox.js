0eacfa873c980c35b01e6d318e97aec7191952a5
Jamal Shamsi <93354040+jamalshamsi1988@users.noreply.github.com>
2023-12-10 17:47:34 +0330

    Project completed

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,41 +1,60 @@
 import React from "react";
 import { Button, Container } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 import { IoSearchSharp } from "react-icons/io5";
 import Form from "react-bootstrap/Form";
+
 
 const SearchBox = ({ setSearch, search, searchProducts }) => {
   return (
     <Container
-      style={{ alignItems: "center", textAlign: "center" }}
-      xs={6}
-      md={4}
-    >
-      <Form.Control
-        style={{
-          display:"inline-block",
-          height: "30px",
-          width: "300px",
-          border: "1px solid silver",
-          borderRadius: " 5px",
-          backgroundColor: "#f6f6f6",
-          padding: "20px",
-          margin: "30px 50px 30px",
-        }}
-        type="text"
-        placeholder="Search Product"
-        value={search}
-        onChange={(e) => setSearch(e.target.value.toLowerCase())}
-        id="inputText"
-        aria-describedby="passwordHelpBlock"
-      />
-      <Button
-        variant="primary"
-        style={{ width: "50px"}}
-        onClick={searchProducts}
+    style={{ alignItems:"center",textAlign:"center" }}
+    xs={6}
+    md={4}
+  >
+      <Dropdown style={{display:"inline-block"}}>
+      <Dropdown.Toggle
+        variant="success"
+        id="dropdown-basic"
+        style={{ backgroundColor: "#0B5ED7"}}
       >
-        <IoSearchSharp />
-      </Button>
-    </Container>
+        Choose Category
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        <Dropdown.Item href="/mens">Mens</Dropdown.Item>
+        <Dropdown.Item href="/womens">Wones</Dropdown.Item>
+        <Dropdown.Item href="/jewelery">Jewelery</Dropdown.Item>
+        <Dropdown.Item href="/electronic">Electronic</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+
+    <Form.Control
+      style={{
+        display:"inline-block",
+        height: "30px",
+        width: "300px",
+        border: "1px solid silver",
+        borderRadius: " 5px",
+        backgroundColor: "#f6f6f6",
+        padding: "18px",
+        margin: "30px 20px 30px",
+      }}
+      type="text"
+      placeholder="Search Product"
+      value={search}
+      onChange={(e) => setSearch(e.target.value.toLowerCase())}
+      id="inputText"
+      aria-describedby="passwordHelpBlock"
+    />
+    <Button
+      variant="primary"
+      style={{ width: "50px"}}
+      onClick={searchProducts}
+    >
+      <IoSearchSharp />
+    </Button>
+  </Container>
   );
 };
 
--- src/components/Store.js
+++ src/components/Store.js
@@ -1,19 +1,19 @@
-import React, { useContext, useEffect, useState} from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import { ProductsContext } from "../context/productContextProvider";
 import { Button, Container } from "react-bootstrap";
-import Dropdown from 'react-bootstrap/Dropdown';
+
 import { shorten } from "../helper/function.js";
 import SearchBox from "./SearchBox.js";
-import { Link } from "react-router-dom";
+
 
 const Store = () => {
   const products = useContext(ProductsContext);
   const [productsData, setProductsData] = useState(products);
   const [search, setSearch] = useState([]);
- 
+
   useEffect(() => setProductsData(products), [products]);
 
   const searchProducts = () => {
@@ -29,29 +29,13 @@ const Store = () => {
 
   return (
     <Container>
+
       <SearchBox
         searchProducts={searchProducts}
         search={search}
         setSearch={setSearch}
       />
-      
-      {/* <Link to="/mens"><Button variant="primary">Mens</Button></Link>
-      <Link to="/womens"><Button variant="primary">Women</Button></Link>
-      <Link to="/jewelery"><Button variant="primary">Jewelery</Button></Link>
-      <Link to="/electronic"><Button variant="primary">Electronics</Button></Link> */}
- <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic" style={{backgroundColor:"#0B5ED7"}}>
-        Choose Category
-      </Dropdown.Toggle>
-
-      <Dropdown.Menu>
-        <Dropdown.Item href="/mens">Mens</Dropdown.Item>
-        <Dropdown.Item href="/womens">Wones</Dropdown.Item>
-        <Dropdown.Item href="/jewelery">Jewelery</Dropdown.Item>
-        <Dropdown.Item href="/electronic">Electronic</Dropdown.Item>
-      </Dropdown.Menu>
-    </Dropdown>
-     
+    
       <Row>
         {productsData.map((product) => (
           <Col xs={6} md={4} key={product.id}>
